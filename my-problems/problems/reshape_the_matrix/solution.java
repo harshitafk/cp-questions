2ffb813cb22a74897602bcca4015d9ce4ca3489e
Harshit <63135557+harshitafk@users.noreply.github.com>
2021-12-18 17:16:53 +0000

    Sync LeetCode submission - Reshape the Matrix (java)

---
+++ my-problems/problems/reshape_the_matrix/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+      int rowSize = mat.length;
+	      int colSize = mat[0].length;
+	      
+	      if((rowSize * colSize) != (r*c)) {
+	    	  return mat;
+	      }else {
+	    	  
+	    	  int[][] inArr = new int[r][c];
+	    	  
+	    	  int k = 0,m= 0,n = 0;
+	    	  for(int i = 0,j=0; i < rowSize*colSize; i++) {
+	    		  if(k > c-1) {
+					  k = 0;
+					  j++;
+				  }
+	    		  
+	    		  if(n > colSize-1) {
+					  n = 0;
+					  m++;
+				  }
+	    		  inArr[j][k] = mat[m][n];
+	    		  k++;
+	    		  n++;
+	    				 
+	    	  }
+	    	  
+	    	  return inArr;
+	      }
+    }
+}
