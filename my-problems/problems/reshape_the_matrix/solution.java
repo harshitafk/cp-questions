f83693e8f7b53c7a4e731e8be752e22640f8bd39
Harshit <63135557+harshitafk@users.noreply.github.com>
2021-12-30 17:42:53 +0000

    Sync LeetCode submission - Reshape the Matrix (java)

--- my-problems/problems/reshape_the_matrix/solution.java
+++ my-problems/problems/reshape_the_matrix/solution.java
@@ -6,9 +6,7 @@ class Solution {
 	      if((rowSize * colSize) != (r*c)) {
 	    	  return mat;
 	      }else {
-	    	  
 	    	  int[][] inArr = new int[r][c];
-	    	  
 	    	  int k = 0,m= 0,n = 0;
 	    	  for(int i = 0,j=0; i < rowSize*colSize; i++) {
 	    		  if(k > c-1) {
