eb9b22fdbfc50ba671070a892c5423a878c67cab
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-03-06 15:09:22 +0000

    Sync LeetCode submission - Minimum Insertion Steps to Make a String Palindrome (java)

---
+++ my-problems/problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+      int[][] dp = new int[n+1][n+1];
+      for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j){
+          if(s.charAt(i) == s.charAt(n - 1 - j)){
+            dp[i+1][j+1] = dp[i][j] + 1;
+          }else{
+             dp[i+1][j+1] = Math.max(dp[i+1][j],dp[i][j+1]);
+          }
+        }
+      }
+      
+      return n - dp[n][n];
+    }
+}
