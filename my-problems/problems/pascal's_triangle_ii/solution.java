cd4fd45d04a1946cd72802f5fceb35399f36d60e
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-03-30 17:27:08 +0000

    Sync LeetCode submission - Pascal's Triangle II (java)

--- my-problems/problems/pascal's_triangle_ii/solution.java
+++ my-problems/problems/pascal's_triangle_ii/solution.java
@@ -1,23 +1,27 @@
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        int[][] row = new int[rowIndex+1][rowIndex+1];
-        List<Integer> list = new ArrayList<>();
-        if(rowIndex == 0) {
-        	 list.add(1);
-        	 return list;
-        }
-        for(int i = 1; i <= rowIndex; i++) {
-        	   row[i][0] = 1; 
-        	for(int j = 1; j < i; j++) {
-        		  row[i][j] = row[i-1][j-1] + row[i-1][j];     		
-        	}
-        	row[i][i] = 1;
-        }
-        
-       for(int i =0; i < row[rowIndex].length; i++) {
-    	   list.add(row[rowIndex][i]);
-       }
-       
-       return list;
-    }
+     public List<Integer> getRow(int rowIndex) {
+	        int dp[][] = new int[rowIndex+1][rowIndex+1];
+	        
+	        dp[0][0] = 1;
+	        List<Integer> list2 = new ArrayList<>();
+	       for(int i = 1; i <= rowIndex; i++){
+	           for(int j = 0; j < i; j++){
+	               if(j == 0){
+	                   dp[i][j] = 1;
+	                   if(i == rowIndex){
+	                       list2.add( dp[i][j]); 
+	                   } 
+	               }else{
+	                   dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+	                   if(i == rowIndex){
+	                        list2.add(dp[i][j]);   
+	                   }
+	               }
+	           }
+	           dp[i][i] = 1;
+	       }
+	         list2.add(dp[rowIndex][rowIndex] = 1);
+	        
+	        return list2;
+	    }
 }
