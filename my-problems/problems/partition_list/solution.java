6a3359c3e8843ebadb696b4c7dc0be759a764c28
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-04-07 18:50:33 +0000

    Sync LeetCode submission - Partition List (java)

---
+++ my-problems/problems/partition_list/solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+      
+      if(head == null) return head;
+      
+      ListNode part1 = new ListNode(0);
+      ListNode part2 = new ListNode(0);
+      ListNode temp1 = part1,temp2 = part2;
+      ListNode curr = head;
+      
+      while(curr != null){
+        if(curr.val >= x){
+          temp2.next = curr;
+          temp2 = curr;
+        }else{
+          temp1.next = curr;
+          temp1 = curr;
+        }
+        curr = curr.next;
+      }
+      
+      temp1.next = part2.next;
+      temp2.next = null;
+      return part1.next;
+      
+    }
+}
