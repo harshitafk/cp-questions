07fc002c7019dbe0d6d82908ec750b747ae5bff1
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-04-07 08:06:59 +0000

    Sync LeetCode submission - Last Stone Weight (java)

--- my-problems/problems/last_stone_weight/solution.java
+++ my-problems/problems/last_stone_weight/solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-       if (stones.length == 0)
-		return 0;
-	Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-	for (int stone : stones)
-		maxHeap.add(stone);
-	while (maxHeap.size() > 1)
-		maxHeap.offer(maxHeap.poll() - maxHeap.poll());
-	return maxHeap.poll();
+      Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+      
+      for(int i : stones) queue.add(i);
+      
+      while(queue.size() > 1){
+        queue.add(queue.poll() - queue.poll());
+      }
+      
+      return queue.poll();
     }
 }
 //25,11,8,7,6,5,1
