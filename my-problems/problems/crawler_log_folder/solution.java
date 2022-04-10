af0f369a00795032581a42f433d99fea452a8101
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-04-10 11:25:58 +0000

    Sync LeetCode submission - Crawler Log Folder (java)

---
+++ my-problems/problems/crawler_log_folder/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stack = new Stack<>();
+      
+      for(String log : logs){
+        
+        if(log.equals("../")){
+          if(!stack.isEmpty()){
+          stack.pop();  
+          }
+        }else if(log.equals("./")){
+          continue;
+        }else{
+          stack.push(log);
+        }
+      }
+      
+      return stack.size();
+    }
+}
