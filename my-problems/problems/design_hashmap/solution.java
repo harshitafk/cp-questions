d0a8cb59b90fc3c76f5dc63a5e451c7a0bbdb2fc
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-04-22 18:06:59 +0000

    Sync LeetCode submission - Design HashMap (java)

--- my-problems/problems/design_hashmap/solution.java
+++ my-problems/problems/design_hashmap/solution.java
@@ -1,66 +1,84 @@
 class MyHashMap {
 
-    LinkedList<Entry>[] map;
+   private LinkedList<Entry>[] map;
     private static int SIZE = 769;
     public MyHashMap() {
-      this.map = new LinkedList[SIZE];
+        this.map = new LinkedList[SIZE];
     }
     
     public void put(int key, int value) {
-      int bucket = key % SIZE;
+        int bucket = key % SIZE;
+      
       if(map[bucket] == null){
         map[bucket] = new LinkedList<Entry>();
         map[bucket].add(new Entry(key,value));
         return;
       }
-        
-        for(Entry entry : map[bucket]){
-          if(entry.key == key){
-            entry.val = value;
-            return;
-          } 
+      
+      for(Entry entry : map[bucket]){
+        if(entry.getKey() == key){
+          entry.setValue(value); 
+          return;
         }
+      }
       
        map[bucket].add(new Entry(key,value));
     }
     
     public int get(int key) {
-      int bucket = key % SIZE;
+        int bucket = key % SIZE;
       
       if(map[bucket] == null) return -1;
-    
-        for(Entry entry : map[bucket]){
-          if(entry.key == key) return entry.val;
-        }
       
-        return -1;
+      for(Entry entry : map[bucket]){
+        if(entry.getKey() == key){
+          return entry.getValue();
+        }
+      }
+      
+      return -1;
     }
     
     public void remove(int key) {
-        int bucket = key % SIZE;
-     
-      if(map[bucket] == null) return;
-       Entry toRemove = null;
+      
+      int bucket = key % SIZE;
+      
+       if(map[bucket] == null) return;
+      
+      Entry toRem = null;
+      
       for(Entry entry : map[bucket]){
-        if(entry.key == key){
-          toRemove = entry;
+        if(entry.getKey() == key){
+         toRem = entry;
+          break;
         }
       }
       
-      if(toRemove != null){
-        map[bucket].remove(toRemove);
-      }
+      if(toRem != null){
+          map[bucket].remove(toRem);
+      }     
     }
   
   class Entry{
-    int key;
-    int val;
+    private int key;
+    private int value;
     
-    Entry(int key, int val){
+   public Entry(int key, int value){
       this.key = key;
-      this.val = val;
+      this.value = value;
     }
     
+    public int getKey(){
+      return this.key;
+    }
+    
+    public int getValue(){
+      return this.value;
+    }
+    
+    public void setValue(int value){
+       this.value = value;
+    }
   }
 }
 
