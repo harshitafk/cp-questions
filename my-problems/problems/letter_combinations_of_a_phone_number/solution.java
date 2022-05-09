6cbdd16b70e759ca833f9732fab6dceb6e650fc6
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-05-09 05:25:21 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (java)

--- my-problems/problems/letter_combinations_of_a_phone_number/solution.java
+++ my-problems/problems/letter_combinations_of_a_phone_number/solution.java
@@ -1,25 +1,26 @@
 class Solution {
-    public List<String> letterCombinations(String digits){
-      	List<String> list = new ArrayList<>();
-		
-        if (digits.length() == 0) return list;
-        
-        String[] dict = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        
-        backtrack(list,dict,digits.toCharArray(),new StringBuilder());  
-        
-        return list;
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+      
+      if(digits.length() == 0) return list;
+      String[] dict = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+      
+      backtrack(list,digits.toCharArray(),dict,new StringBuilder());
+      
+      return list;
     }
-	
-	private void backtrack(List<String> list, String[] dict, char[] digitsChar, StringBuilder s) {
-		if(s.length() == digitsChar.length) { list.add(s.toString()); return;}
-		
-		int length = s.length();
-		int index = digitsChar[length] - '0';
-		for(char letter : dict[index].toCharArray()) {
-			backtrack(list,dict,digitsChar,s.append(Character.toString(letter)));
-			s.deleteCharAt(s.length() - 1);
-		}	
-	}
   
+  public void backtrack(List<String> list, char[] digits, String[] dict, StringBuilder builder){
+    if(builder.length() == digits.length){ list.add(builder.toString()); 
+                                     return;}
+    
+    int n = builder.length();
+    int dig = digits[n] - '0';
+    
+    for(char c : dict[dig].toCharArray()){
+      backtrack(list,digits,dict,builder.append(Character.toString(c)));
+      builder.deleteCharAt(builder.length()-1);
+    }
+    
+  }
 }
