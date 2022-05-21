871bca1f9497b108d0990f39776fe0c2671e5d89
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-05-21 19:27:05 +0000

    Sync LeetCode submission - Coin Change (java)

--- my-problems/problems/coin_change/solution.java
+++ my-problems/problems/coin_change/solution.java
@@ -1,12 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-       int[] dp = new int[amount + 1];
-    for (int i=1; i < dp.length; i++) {
-      dp[i] = dp.length;
-      for (int j=0; j < coins.length; j++) {
-        if (i >= coins[j]) dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
-      }
-    }
-    return dp[amount] == dp.length ? -1 : dp[amount];
+        
+        int n = coins.length;
+        
+        int[][] dp = new int[n+1][amount+1];
+        
+        for(int i = 0; i < amount+1; i++){
+            dp[0][i] = Integer.MAX_VALUE - 1;  //
+            
+            if(i%coins[0] == 0){
+                dp[1][i] = i/coins[0];
+            }else{
+                dp[1][i] = Integer.MAX_VALUE - 1;
+            }
+        }
+        
+        for(int i = 2; i < n+1; i++){
+            for(int j = 1; j < amount+1; j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] = Math.min((1+dp[i][j - coins[i-1]]),dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[coins.length][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[coins.length][amount];
+        
     }
 }
