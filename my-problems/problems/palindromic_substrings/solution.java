9131f5bec409be326f669ccea2057c6326329bdb
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-05-22 20:20:39 +0000

    Sync LeetCode submission - Palindromic Substrings (java)

---
+++ my-problems/problems/palindromic_substrings/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countSubstrings(String s) {
+     var n = s.length();
+	var dp = new boolean[n][n];
+	for (var i = 0; i < n; i++)
+		dp[i][i] = true;
+
+	var count = n;
+	for (var i = 0; i + 1 < n; i++)
+		if (s.charAt(i) == s.charAt(i + 1)) {
+			dp[i][i + 1] = true;
+			count++;
+		}
+
+	for (var len = 2; len < n; len++) // controls the size of the substring
+		for (var i = 0; i + len < n; i++) { // controls the start index
+			var j = i + len; // end index
+			if ((s.charAt(i) == s.charAt(j)) && dp[i + 1][j - 1]) {
+				dp[i][j] = true;
+				count++;
+			}
+		}
+	return count;
+    }
+}
