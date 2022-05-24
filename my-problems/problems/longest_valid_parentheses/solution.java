4e4ab58c66cdeeceecc57e8d45702f373a74215e
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-05-24 15:11:20 +0000

    Sync LeetCode submission - Longest Valid Parentheses (java)

--- my-problems/problems/longest_valid_parentheses/solution.java
+++ my-problems/problems/longest_valid_parentheses/solution.java
@@ -1,20 +1,24 @@
 class Solution {
     public int longestValidParentheses(String s) {
-   Deque<Integer> stack = new ArrayDeque<>(s.length());
-		stack.push(-1);
-		int max = 0;
-		for(int i = 0; i< s.length(); i++) {
-			if(s.charAt(i) == '(') stack.push(i);
-			else {
-				stack.pop();
-				if(stack.isEmpty()) {
-					stack.push(i);
-				}else {
-					max = Math.max(max, i - stack.peek());
-				}
-			}
-				
-		}
-		return max;
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(-1);
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(i);
+            }else{
+                stack.pop();
+                
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }else{
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        
+        return max;
     }
 }
