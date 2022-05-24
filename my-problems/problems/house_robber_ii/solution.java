a1634d0417cea0228c5a07577cde67552778510b
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-05-24 20:24:58 +0000

    Sync LeetCode submission - House Robber II (java)

--- my-problems/problems/house_robber_ii/solution.java
+++ my-problems/problems/house_robber_ii/solution.java
@@ -1,33 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-         if(nums.length == 0) return 0;
-      
-      if(nums.length == 1) return nums[0];
-      
-      if(nums.length == 2) return Math.max(nums[0],nums[1]);
-      
-      return Math.max(max1(nums, new int[nums.length]), max2(nums, new int[nums.length]));
-     
+        
+        if(nums.length == 1) return nums[0];
+        
+        return Math.max(rob(nums,0,nums.length-1),rob(nums,1,nums.length));
     }
-  
-  private int max1(int[] nums,int[] dp){
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0],nums[1]);
-     for(int i = 2; i < nums.length-1; i++){
-        int val = nums[i];
-        dp[i] = Math.max(dp[i-1],dp[i-2] + val);
-      }
     
-    return dp[nums.length - 2];
-  }
-  
-  private int max2(int[] nums,int[] dp){
-    dp[1] = nums[1];
-     for(int i = 2; i < nums.length; i++){
-        int val = nums[i];
-        dp[i] = Math.max(dp[i-1],dp[i-2] + val);
-      }
-    
-    return dp[nums.length - 1];
-  }
+     public int rob(int[] nums,int l, int r) {
+        //int[] dp = new int[nums.length + 1];
+         
+         int curr = 0,prev = 0;
+        
+        for(int i = l; i < r; i++){
+            int t = curr;
+            curr = Math.max(prev + nums[i], curr);
+            prev = t;
+        }
+        
+        return curr;
+    }
 }
