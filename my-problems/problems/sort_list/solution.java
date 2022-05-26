b185533c772a7990740d41ab1a8e4ad8d40838b4
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-05-26 17:44:34 +0000

    Sync LeetCode submission - Sort List (java)

--- my-problems/problems/sort_list/solution.java
+++ my-problems/problems/sort_list/solution.java
@@ -10,22 +10,53 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-      
-      while(head != null){
-        list.add(head.val);
-        head = head.next;
-      }
-      
-      list.sort((a,b) -> a.compareTo(b));
-      
-      ListNode node = new ListNode(0);
-      ListNode temp = node;
-      for(int i = 0; i < list.size(); i++){
-        temp.next = new ListNode(list.get(i));
-        temp = temp.next;
-      }
-      
-      return node.next;
+        
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode left, ListNode right){
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        
+        while(left != null && right != null){
+            if(left.val < right.val){
+                tail.next = left;
+                left = left.next;
+                tail = tail.next;
+            }else{
+                tail.next = right;
+                right = right.next;
+                tail = tail.next;
+            }
+            
+        }
+        
+        tail.next = (left != null) ? left : right;
+        
+        return dummy.next;
+    }
+    
+    private ListNode getMid(ListNode head){
+        
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        prev.next = null;
+        return slow;
     }
 }
