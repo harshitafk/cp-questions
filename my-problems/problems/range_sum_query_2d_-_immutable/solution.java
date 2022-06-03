56312737892e3b13567d71d954994a4ab1626cdc
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-06-03 19:16:15 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (java)

--- my-problems/problems/range_sum_query_2d_-_immutable/solution.java
+++ my-problems/problems/range_sum_query_2d_-_immutable/solution.java
@@ -1,32 +1,19 @@
 class NumMatrix {
 
-    int[][] dp = null;
+    private int[][] dp;
     public NumMatrix(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-      
-        dp = new int[r+1][c+1];
-      
-      dp[1][1] = matrix[0][0];
-      for(int i = 1; i < matrix[0].length; i++){
-        dp[1][i+1] = matrix[0][i] + dp[1][i];
-      }
-      
-      for(int i = 1; i < matrix.length; i++){
-        for(int j = 0; j < matrix[0].length; j++){
-          if(j == 0){
-          dp[i+1][j+1] = dp[i][j+1] + matrix[i][j];  
-          }else{
-            dp[i+1][j+1] = matrix[i][j] + dp[i+1][j] + dp[i][j+1] - dp[i][j];
-          }
+    if (matrix.length == 0 || matrix[0].length == 0) return;
+    dp = new int[matrix.length + 1][matrix[0].length + 1];
+    for (int r = 0; r < matrix.length; r++) {
+        for (int c = 0; c < matrix[0].length; c++) {
+            dp[r + 1][c + 1] = dp[r + 1][c] + dp[r][c + 1] + matrix[r][c] - dp[r][c];
         }
-      }
     }
-    
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-     
-        return dp[row2+1][col2+1] - dp[row1][col2+1] - dp[row2+1][col1] + dp[row1][col1];
-    }
+}
+
+public int sumRegion(int row1, int col1, int row2, int col2) {
+    return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+}
 }
 
 /**
