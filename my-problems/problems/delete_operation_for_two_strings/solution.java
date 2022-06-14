e6350a3f694a0b3e45c369be4dcb8f7e74d62e98
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-06-14 13:31:07 +0000

    Sync LeetCode submission - Delete Operation for Two Strings (java)

--- my-problems/problems/delete_operation_for_two_strings/solution.java
+++ my-problems/problems/delete_operation_for_two_strings/solution.java
@@ -1,20 +1,15 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-         int word1length = word1.length();
-        int word2length = word2.length();
         
-        int[][] dp = new int[word1length + 1][word2length + 1];
+        int n = word1.length(), m = word2.length();
+        int[][] dp = new int[n+1][m+1];
         
-        for(int i = 1; i < word1length + 1; i++){
-            for(int j = 1; j < word2length + 1; j++){
-            if(word1.charAt(i-1) == word2.charAt(j-1)){
-                dp[i][j] = 1 + dp[i-1][j-1];
-            }else{
-                dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                dp[i][j] = word1.charAt(i-1) == word2.charAt(j-1) ? dp[i-1][j-1] + 1 : Math.max(dp[i-1][j],dp[i][j-1]);
             }
-          }
         }
         
-        return (word1length - dp[word1length][word2length]) + (word2length - dp[word1length][word2length]);
+        return n + m - (2* dp[n][m]);
     }
 }
