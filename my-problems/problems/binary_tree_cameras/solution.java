cb755a44b5ffb8ae95a25e9f81f146ab76a2adc3
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-06-17 20:07:03 +0000

    Sync LeetCode submission - Binary Tree Cameras (java)

---
+++ my-problems/problems/binary_tree_cameras/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int cam = 0;
+    public int minCameraCover(TreeNode root) {
+        return dfs(root) == 0 ? cam+1 : cam;
+    }
+    
+    private int dfs(TreeNode root){
+        if(root == null) return 1;
+        
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        
+        if(left == 0 || right == 0){
+            cam++;
+            return 2;
+        }else if(left == 2 || right == 2) return 1;
+        else return 0;
+    }
+    
+    
+}
