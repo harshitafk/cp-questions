709e5dbfb61c2dcfc52d29ad59f2006bd25ec1cb
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-06-29 17:04:09 +0000

    Sync LeetCode submission - Sort Colors (java)

--- my-problems/problems/sort_colors/solution.java
+++ my-problems/problems/sort_colors/solution.java
@@ -1,42 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int zero=0,one=0,two = 0;
-        for(int i = 0; i< nums.length; i++){
-            if(nums[i] == 0){
-                map.put(nums[i],++zero);
-            }else if(nums[i] == 1){
-                 map.put(nums[i],++one);
-            }else{
-                map.put(nums[i],++two);
+        int p1 = 0, index = 0, p2 = nums.length - 1;
+        
+        while(index <= p2){
+            
+            if(nums[index] == 0){
+                nums[index] = nums[p1];
+                nums[p1] = 0;
+                p1++;
             }
             
-        }
-        
-         zero=0;one=0;two = 0;
-        if(map.containsKey(0)){
-             zero = map.get(0);
-        }
-        
-        if(map.containsKey(1)){
-             one = map.get(1);
-        }
-        
-         if(map.containsKey(2)){
-             two = map.get(2);
-        }
-       
-        for(int i = 0; i < zero; i++){
-            nums[i] = 0;
-        }
-        
-        for(int i = zero; i < one+zero; i++){
-            nums[i] = 1;
-        }
-        
-        for(int i = one+zero; i <  one+zero+two; i++){
-              nums[i] = 2;
-
+            if(nums[index] == 2){
+                nums[index] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                index--;
+            }
+            
+            index++;
         }
     }
 }
