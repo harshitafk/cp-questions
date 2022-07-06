5d9504d6a30323a7f24f91d8d222c729e4c37d7f
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-07-06 05:15:18 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- my-problems/problems/fibonacci_number/solution.java
+++ my-problems/problems/fibonacci_number/solution.java
@@ -1,14 +1,10 @@
 class Solution {
     public int fib(int n) {
-      int[] dp = new int[n+1];
-       return fibonacci(n,dp);
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        
+        int sum = fib(n-1) + fib(n-2);
+        
+        return sum;
     }
-  
-  private int fibonacci(int n, int[] dp){
-        if(n <= 0) return 0;
-	      if(n == 1) return 1;
-	      dp[n] = fib(n-1) + fib(n-2);
-    
-       return dp[n];
-  }
 }
