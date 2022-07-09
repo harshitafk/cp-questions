26f3614f5babca353c239a08502b433b68f23d28
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-07-09 20:37:38 +0000

    Sync LeetCode submission - Perfect Squares (java)

---
+++ my-problems/problems/perfect_squares/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSquares(int n) {
+       int[] dp = new int[n+1];
+        
+        for(int i = 1; i <= n; i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 1; j * j <= i; j++){
+                min = Math.min(min,dp[i - (j*j)] + 1);
+            }
+            dp[i] = min;
+        }
+        
+        return dp[n];
+    }
+}
