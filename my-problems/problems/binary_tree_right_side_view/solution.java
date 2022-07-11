f27dd029c8fed176584986b08a57f2199e5a8528
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-07-11 17:06:55 +0000

    Sync LeetCode submission - Binary Tree Right Side View (java)

--- my-problems/problems/binary_tree_right_side_view/solution.java
+++ my-problems/problems/binary_tree_right_side_view/solution.java
@@ -15,20 +15,19 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
         
-      List<Integer> list = new ArrayList<>();
-      getView(list,root,0);
-      
-      return list;
+        sideView(root,list,0);
+        
+        return list;
     }
-  
-  private void getView(List<Integer> list,TreeNode root,int level){
     
-    if(root == null) return;
-    
-    if(list.size() == level) list.add(root.val);
-    
-    getView(list,root.right,level+1);
-    getView(list,root.left,level+1);
-  }
+    public void sideView(TreeNode root,List<Integer> list,int level){
+        if(root == null) return;
+        
+        if(list.size() == level) list.add(root.val);
+        
+        sideView(root.right,list,level+1);
+        sideView(root.left,list,level+1);
+    }
 }
