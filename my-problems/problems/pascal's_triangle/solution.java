1f2a18bd04f83acfc02c9a40f4c75e32526eecef
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-07-19 14:35:48 +0000

    Sync LeetCode submission - Pascal's Triangle (java)

--- my-problems/problems/pascal's_triangle/solution.java
+++ my-problems/problems/pascal's_triangle/solution.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        int dp[][] = new int[numRows][numRows];
+        List<List<Integer>> listOfList = new ArrayList<>();
+        listOfList.add(Arrays.asList(1));
         
-        List<List<Integer>> list = new ArrayList<>();
-        list.add(List.of(1));
+        for(int i = 1; i < numRows; i++){
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            
+             
+            for(int j = 0; j < i-1; j++){    
+                List<Integer> getList = listOfList.get(i-1);
+                int sum = getList.get(j) + getList.get(j+1);
+                if(list.add(sum));  
+            }
+            
+            list.add(1);
+            listOfList.add(list);
+        }
         
-        dp[0][0] = 1;
-       for(int i = 1; i < numRows; i++){
-           List<Integer> list2 = new ArrayList<>();
-           for(int j = 0; j < i; j++){
-               if(j == 0){
-                    list2.add(dp[i][j] = 1);
-               }else{
-                list2.add(dp[i][j] = dp[i-1][j-1] + dp[i-1][j]);   
-               }
-           }
-           list2.add(dp[i][i] = 1);
-           
-           list.add(list2);
-       }
-        
-        return list;
+        return listOfList;
     }
 }
