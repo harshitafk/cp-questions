a3a774087d3ca39a4ce949379bd13c81d9410387
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-07-20 15:15:59 +0000

    Sync LeetCode submission - Number of Matching Subsequences (java)

--- my-problems/problems/number_of_matching_subsequences/solution.java
+++ my-problems/problems/number_of_matching_subsequences/solution.java
@@ -1,38 +1,34 @@
 class Solution {
     public int numMatchingSubseq(String s, String[] words) {
-       
-        int result = 0;
-         int n = s.length();
-         HashMap<String,Boolean> map = new HashMap<>();
-        for(String word : words){
-            
-            if(!map.containsKey(word)){
-              int m = word.length();
-              if(longestCommonSubsequence(s, word,n,m) == m) {
-                  map.put(word,true);
-                  result++;
-              }else{
-                  map.put(word,false); 
-              }
-            }else{
-                if(map.get(word)){
-                 result++;
-                }
-            }
-           
-        }
-      
-      return result;
+             int result = 0;
+	      
+	      Map<String,Integer> map = new HashMap<>();
+	      
+	      for(String word : words){
+	        map.put(word,map.getOrDefault(word,0)+1);
+	      }
+	      
+	      char[] schar = s.toCharArray();
+	      
+	      for(String word : map.keySet()){
+	        char[] wordchar = word.toCharArray();
+	        int m = word.length();
+	        
+	        int j = 0;
+	        
+	        for(int i = 0; i < s.length() && j < m; i++) {
+	        	if(schar[i] == wordchar[j]) {
+	        		j++;
+	        	}
+	        }
+	        
+	        if(j == m) {
+	        	result += map.get(word);
+	        }
+	        
+	        
+	      }
+	      
+	      return result;
     }
-  
-     public int longestCommonSubsequence(String text1, String text2,int n, int m) {
-       int j = 0;
-        for (int i = 0; i < n && j < m; i++){
-            if (text2.charAt(j) == text1.charAt(i))
-                j++;
-        }
-         
-       return j;
-        
-  }
 }
