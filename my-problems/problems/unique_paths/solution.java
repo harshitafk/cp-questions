ff8ed5af6ae80841cac27a24b25fca600b247d76
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-08-01 14:54:54 +0000

    Sync LeetCode submission - Unique Paths (java)

--- my-problems/problems/unique_paths/solution.java
+++ my-problems/problems/unique_paths/solution.java
@@ -1,21 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       int[][] dp = new int[m][n];
         
-        for(int i = 0; i < n; i++){
-            dp[0][i] = 1;
+        long power = 2l * (long) Math.pow(10,9);
+        
+        int[][] dp = new int[m][n];
+        
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
         }
         
-        for(int i = 1; i < m; i++){
-            dp[i][0] = 1;
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
         }
         
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[i][j] = (int) ((dp[i-1][j] + dp[i][j-1])%power);
             }
         }
         
-        return dp[m-1][n-1]; 
+        return dp[m-1][n-1];
     }
 }
