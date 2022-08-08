3a38f5d731f63b45cf6bd313b107d52faf9239d9
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-08-08 06:11:18 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- my-problems/problems/longest_increasing_subsequence/solution.java
+++ my-problems/problems/longest_increasing_subsequence/solution.java
@@ -1,16 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        TreeSet<Integer> tree = new TreeSet<>();
-      
-      for(int i = 0; i < nums.length; i++){   //10,9,2,5,3,7,101,18 -> 10,9,2   5,3    7    101,18
-        Integer x = tree.ceiling(nums[i]);  //10 --> null  9 --> 10   2-->10   5-->null   3-->5   7-->null   101-->null     18-->101 
+        TreeSet<Integer> set = new TreeSet<>();
         
-        if(x != null){              
-          tree.remove(x);                  //                 []       []       [2]        [2]      [2,3]      [2,3,7]       [2,3,7]
+        for(int i = 0; i < nums.length; i++){
+        Integer val = set.ceiling(nums[i]);
+            
+        if(val != null){
+         set.remove(val);   
         }
-        tree.add(nums[i]);                //[10]             [9]      [2]      [2,5]       [2,3]    [2,3,7]    [2,3,7,101]   [2,3,7,18]
-      }
-      
-      return tree.size();
+            
+        set.add(nums[i]);
+        
+        }
+        
+        return set.size();
     }
 }
