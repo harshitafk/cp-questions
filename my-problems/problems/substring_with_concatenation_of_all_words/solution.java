11628570708c01bf36080e7e3b6566b218e6d994
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-08-13 20:13:10 +0000

    Sync LeetCode submission - Substring with Concatenation of All Words (java)

--- my-problems/problems/substring_with_concatenation_of_all_words/solution.java
+++ my-problems/problems/substring_with_concatenation_of_all_words/solution.java
@@ -1,42 +1,39 @@
 class Solution {
-  
-    private Map<String,Integer> map = new HashMap<>();
-    private int sLen;
-    private int arrSize;
-    private int wordLen;
-    private int subStringLength;
     public List<Integer> findSubstring(String s, String[] words) {
-      sLen = s.length();
-      arrSize = words.length;
-      wordLen = words[0].length();
-      subStringLength = wordLen * arrSize;
-      
-      for(String word : words) map.put(word,  map.getOrDefault(word, 0) + 1);
-      
-      List<Integer> list = new ArrayList<>();
-      for(int i = 0; i < sLen - subStringLength + 1; i++){
-        if(helper(s,i)){
-          list.add(i);
+        List<Integer> list = new ArrayList<>();
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
-      }
-      
-      return list;
+
+        int len = s.length(), wordsLen = words.length, wLen = words[0].length(),
+                totalLen = wordsLen * wLen;
+
+        for (int i = 0; i < len - totalLen + 1; i++) {
+            Map<String, Integer> seen = new HashMap<>();
+
+            int  j = 0;
+
+            while(j < wordsLen){
+                String word = s.substring(i + j*wLen, i + (j + 1)*wLen);
+                
+                if(map.containsKey(word)){
+                    seen.put(word,seen.getOrDefault(word,0)+1);
+                    if(seen.get(word) > map.getOrDefault(word,0)){
+                        break;
+                    }
+                }else{
+                    break;
+                }
+                j++;
+            }
+            if(j ==wordsLen){
+                list.add(i);
+            }
+        }
+
+
+        return list;
     }
-  
-  private boolean helper(String s, int i){
-    Map<String,Integer> rmap = new HashMap<>(map);
-    int count = 0;
-    for(int j = i; j < i + subStringLength; j+=wordLen){
-      String sub = s.substring(j,j + wordLen);
-      
-      if(rmap.getOrDefault(sub, 0) != 0){
-        rmap.put(sub, rmap.get(sub) - 1);
-        count++;
-      }else{
-        break;
-      }
-    }
-    
-    return count == arrSize;
-  }
 }
