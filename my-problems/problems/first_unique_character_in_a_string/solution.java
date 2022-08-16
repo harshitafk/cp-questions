48aff79afab60234f58389faeeab9cae5682015a
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-08-16 17:53:40 +0000

    Sync LeetCode submission - First Unique Character in a String (java)

--- my-problems/problems/first_unique_character_in_a_string/solution.java
+++ my-problems/problems/first_unique_character_in_a_string/solution.java
@@ -1,22 +1,23 @@
 class Solution {
     public int firstUniqChar(String s) {
-        	Map<Character,Integer> map = new HashMap<>();
-		for(int i = 0; i < s.length(); i++) {
-			if(map.get(s.charAt(i))== null) {
-				map.put(s.charAt(i), i);
-			}else {
-				map.put(s.charAt(i), i+10000000);
-			}
-		}
-		
-		
-		List<Integer> list = new ArrayList<>(map.values());
-		list.sort((a,b) -> a.compareTo(b));
-		
-		if(list.get(0) > 10000000) {
-			return -1;
-		}else {
-			return list.get(0);
-		}
+        int[] ch = new int[26];
+        
+        Arrays.fill(ch,-1);
+        
+        for(int i = 0; i < s.length(); i++){
+            if(ch[s.charAt(i) - 'a'] == -1) ch[s.charAt(i) - 'a'] = i;
+            else
+                ch[s.charAt(i) - 'a'] = -2;
+        }
+        
+        int res = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < 26; i++){
+            if(ch[i] >= 0){
+            res = Math.min(res,ch[i]);    
+            }
+        }
+        
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 }
