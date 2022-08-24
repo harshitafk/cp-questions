0358cb26e1b8a226a26fb92286f338f79c76664d
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-08-24 17:39:54 +0000

    Sync LeetCode submission - Power of Three (java)

--- my-problems/problems/power_of_three/solution.java
+++ my-problems/problems/power_of_three/solution.java
@@ -1,11 +1,6 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n < 1) return false;
-        
-        if(n == 1) return true;
-        
-        if((n%3) != 0) return false;
-        
-        return isPowerOfThree(n/3);
+        return (n > 0 && 1162261467%n == 0);
+           
     }
 }
