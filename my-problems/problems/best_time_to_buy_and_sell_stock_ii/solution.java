a90e1a81d076867411398af6dbb32ca6aa6fe2d5
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-09-11 10:32:25 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (java)

--- my-problems/problems/best_time_to_buy_and_sell_stock_ii/solution.java
+++ my-problems/problems/best_time_to_buy_and_sell_stock_ii/solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-      if(prices.length == 0) return 0;
-      
-      int lastBuy = -prices[0]; //If you are buying first time then you have to take debt of same amount and buy it,so your 
-                                // actual current balance is in negative.
-      int lastSold = 0;
-      
-      for(int i = 0; i < prices.length; i++){
-        int currBuy = Math.max(lastBuy, lastSold - prices[i]);
-        int currSold = Math.max(lastSold, lastBuy + prices[i]);
+        int profit = 0;
         
-        lastBuy = currBuy;
-        lastSold = currSold;
-      }
-      
-      return lastSold;
+        for(int i = 1; i < prices.length; i++){
+            
+            if(prices[i] > prices[i-1]) profit += prices[i] - prices[i-1]; 
+        }
+        
+        return profit;
     }
 }
