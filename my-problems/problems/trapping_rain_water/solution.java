2a8a4d438da2982f7503cf0b91cc1a9bf8a38077
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-09-18 18:01:26 +0000

    Sync LeetCode submission - Trapping Rain Water (java)

--- my-problems/problems/trapping_rain_water/solution.java
+++ my-problems/problems/trapping_rain_water/solution.java
@@ -1,21 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-      int ans = 0;  
-      int[] leftMax = new int[height.length];
-      int[] rightMax = new int[height.length];
-      leftMax[0] = height[0];
-      rightMax[height.length - 1] = height[height.length - 1];
-      
-      for(int i = 1, j = height.length - 2; i < height.length; ++i,--j){
-        leftMax[i] = Math.max(height[i], leftMax[i-1]);
-        rightMax[j] = Math.max(height[j], rightMax[j+1]);
-      }
-      
-      for(int i = 1; i < height.length - 1; i++){
-        int water = Math.min(leftMax[i - 1],rightMax[i + 1]) - height[i];
-        ans += water > 0 ? water : 0;
-      }
-      
-      return ans;
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        
+        int left = 0;
+        for(int i = 0; i < leftMax.length; i++){
+            leftMax[i] = Math.max(height[i],left);
+            left = leftMax[i];
+        }
+        
+        int right = 0;
+        for(int i = rightMax.length-1; i >= 0; i--){
+            rightMax[i] = Math.max(height[i],right);
+            right = rightMax[i];
+        }
+        
+        int count = 0;
+        
+        for(int i = 0; i < height.length; i++){
+            count += Math.min(leftMax[i],rightMax[i]) - height[i];
+        }
+        
+        return count;
     }
 }
