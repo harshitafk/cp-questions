bb22307d0623ad71596bf9b4ed56943db757eadd
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-10-12 18:35:30 +0000

    Sync LeetCode submission - Largest Perimeter Triangle (java)

--- my-problems/problems/largest_perimeter_triangle/solution.java
+++ my-problems/problems/largest_perimeter_triangle/solution.java
@@ -1,16 +1,13 @@
 class Solution {
     public int largestPerimeter(int[] nums) {
-      Arrays.sort(nums);  
-      for(int i = nums.length-1; i >= 2; i--){
-          if(possible(nums[i],nums[i-1],nums[i-2]))
-            return nums[i]+nums[i-1]+nums[i-2];
+        
+        Arrays.sort(nums);
+        for(int i = nums.length-1; i > 1; i--){
+            if(nums[i] < nums[i-1] + nums[i-2]){
+                return nums[i] + nums[i-1] + nums[i-2];
+            }
         }
-      
-      return 0;
+        
+        return 0;
     }
-  
-  private boolean possible(int a, int b, int c){
-    if(a+b > c && b+c > a && c+a > b) return true;
-    else return false;
-  }
 }
