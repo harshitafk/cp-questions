fe757cbc3191702f07fd5dea2e5cb88fac480174
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-10-15 17:19:56 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- my-problems/problems/climbing_stairs/solution.java
+++ my-problems/problems/climbing_stairs/solution.java
@@ -1,16 +1,20 @@
 class Solution {
-
     public int climbStairs(int n) {
-        if(n == 0 || n == 1 || n == 2) 
-          return n;
-      
-      int[] a = new int[n];
-      a[0] = 1;
-      a[1] = 2;
-      for(int i = 2; i < n; i++){
-        a[i] = a[i-1] + a[i-2];
-      }
-      
-      return a[n-1];
+        
+        
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
 }
