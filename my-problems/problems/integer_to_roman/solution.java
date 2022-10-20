8873405334afccf3176aeeb0fbede598e24d9ba7
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-10-20 17:52:37 +0000

    Sync LeetCode submission - Integer to Roman (java)

--- my-problems/problems/integer_to_roman/solution.java
+++ my-problems/problems/integer_to_roman/solution.java
@@ -1,9 +1,18 @@
 class Solution {
     public String intToRoman(int num) {
-       String M[] = {"", "M", "MM", "MMM"};
-    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(num > 0){
+            while(num >= values[i]){
+                num -= values[i];
+                sb.append(roman[i]);
+            }
+            i++;
+        }
+        
+        return sb.toString();
     }
 }
