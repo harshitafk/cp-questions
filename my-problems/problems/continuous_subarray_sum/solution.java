e3dbb19d9cccf2364bd5d267b30b632597c0f041
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-10-26 21:06:23 +0000

    Sync LeetCode submission - Continuous Subarray Sum (java)

--- my-problems/problems/continuous_subarray_sum/solution.java
+++ my-problems/problems/continuous_subarray_sum/solution.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-     Map<Integer,Integer> map = new HashMap<>();
-		map.put(0,-1);
-		int sum = 0;
-		for(int i = 0; i < nums.length; i++) {
-			sum += nums[i];
-			
-		  sum %= k;
-			Integer val = map.get(sum);
-			
-			if(val != null) {
-				if(i - val > 1) return true;
-			}else map.put(sum,i);
-		}
-		
-		return false;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+      
+      int sum = 0;
+      for(int i = 0; i < nums.length; i++){
+        sum += nums[i];
+        sum %= k;
+        
+        Integer rem = map.get(sum);
+        
+        if(rem != null){
+          if(i - rem > 1) return true;
+        }else{
+          map.put(sum,i);
+        }
+      }
+      
+      return false;
     }
 }
