158bff20c9a49294d109383ccfcdeb3416184dad
Harshit <63135557+harshitafk@users.noreply.github.com>
2022-11-10 16:57:37 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates In String (java)

--- my-problems/problems/remove_all_adjacent_duplicates_in_string/solution.java
+++ my-problems/problems/remove_all_adjacent_duplicates_in_string/solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public String removeDuplicates(String s) {
-       StringBuilder sb = new StringBuilder();
-        int count = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(sb.isEmpty()){
-                sb.append(s.charAt(i));
-                count++;
-            }else if(sb.charAt(count-1) == s.charAt(i)) {
-            	sb.deleteCharAt(count-1);
-            	count--;
-            }else {
-            	sb.append(s.charAt(i));
-            	count++;
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for(int i = 1; i < s.length(); i++){
+            if(!stack.isEmpty() && s.charAt(i) == stack.peek()){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(i));
             }
         }
         
-        
-        return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        for(char c : stack){
+            sb.append(c);
         }
+
+        return sb.toString();
+
+    }
 }
