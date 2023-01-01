2954ebef0ca4fbd01043d589619e2a45dae3e17c
Harshit <63135557+harshitafk@users.noreply.github.com>
2023-01-01 19:00:06 +0000

    Sync LeetCode submission - Word Pattern (java)

--- my-problems/problems/word_pattern/solution.java
+++ my-problems/problems/word_pattern/solution.java
@@ -1,12 +1,17 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-String[] words = s.split(" ");
-    if (words.length != pattern.length())
-        return false;
-    Map index = new HashMap();
-    for (Integer i=0; i<words.length; ++i)
-        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+        String[] words = s.split(" ");
+
+        if(words.length != pattern.length()){
             return false;
-    return true;
+        }
+        Map map = new HashMap();
+        for(Integer i = 0; i < words.length; i++){
+            if(map.put(pattern.charAt(i),i) != map.put(words[i],i)){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
