edfd0f5c74b1e85cbaad5673302e7a96bf16e684
Harshit <63135557+harshitafk@users.noreply.github.com>
2023-01-23 07:11:52 +0000

    Sync LeetCode submission - Palindrome Partitioning (java)

---
+++ my-problems/problems/palindrome_partitioning/solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        if(s == null || s.length() == 0) return new ArrayList<>();
+
+        List<List<String>> result = new ArrayList<>();
+
+        helper(s,new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void helper(String s, List<String> step, List<List<String>> result){
+
+        if(s == null || s.length() == 0){
+            result.add(new ArrayList<>(step));
+            return;
+        }
+
+        for(int i =1; i <= s.length(); i++){
+            String temp = s.substring(0,i);
+            if(!isPalindrome(temp)) continue;
+
+            step.add(temp);
+            helper(s.substring(i,s.length()),step,result);
+            step.remove(step.size()-1);
+        }
+
+    }
+
+    private boolean isPalindrome(String s){
+        int left = 0, right = s.length()-1;
+
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+
+            left++; right--;
+        }
+
+
+        return true;
+    }
+}
