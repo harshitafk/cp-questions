680392ac3348b47df1d41cf633bec626231bae19
Harshit <63135557+harshitafk@users.noreply.github.com>
2023-10-29 18:33:19 +0000

    Sync LeetCode submission - Count Good Nodes in Binary Tree (java)

---
+++ my-problems/problems/count_good_nodes_in_binary_tree/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int goodNodes(TreeNode root) {
+        countN(root, Integer.MIN_VALUE);
+        return count;
+    }
+
+    private void countN(TreeNode root, int max){
+        if(root == null) return ;
+
+        if(root.val >= max) count++;
+
+        max = Math.max(root.val, max);
+
+        countN(root.left, max); 
+        countN(root.right, max);
+    }
+}
